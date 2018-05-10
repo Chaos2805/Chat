c58b6a049420550c2acbf967eecaa7cf08015aa4
Chin Yung Nam <chin@travelog.com>
2018-05-11 00:41:42 +0800

    1st commit

---
+++ app/src/main/java/com/mazetireal/chat/UsersActivity.java
@@ -0,0 +1,120 @@
+package com.mazetireal.chat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    private RecyclerView mUsersList;
+
+    private DatabaseReference mUsersDatabase;
+
+    private LinearLayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        mToolbar =(Toolbar) findViewById(R.id.users_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("All Users");
+
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startListening();
+
+    }
+    public void startListening(){
+        Query query = mUsersDatabase
+//                .getReference()
+//                .child("Users")
+                .limitToLast(50);
+
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(query, Users.class)
+                        .build();
+        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Users, UserViewHolder>(options) {
+            @Override
+            public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.users_single_layout, parent, false);
+
+                return new UserViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(UserViewHolder holder, int position, Users users) {
+                holder.setName(users.getName());
+                holder.setStatus(users.getStatus());
+                holder.setImage(users.getThumb_image());
+
+                final String user_id = getRef(position).getKey();
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent profileIntent = new Intent(UsersActivity.this, ProfileActivity.class);
+                        profileIntent.putExtra("user_id", user_id);
+                        startActivity(profileIntent);
+                    }
+                });
+            }
+
+        };
+        mUsersList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setName(String name){
+            TextView userNameView = (TextView) mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+        }
+        public void setStatus(String status){
+            TextView userStatusView = (TextView) mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(status);
+        }
+        public void setImage(String thumb_image){
+            CircleImageView userImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+            Picasso.get().load(thumb_image).placeholder(R.drawable.ic_avatar).into(userImageView);
+        }
+    }
+}
