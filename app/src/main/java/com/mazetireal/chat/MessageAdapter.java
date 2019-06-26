ee47132937cefe6d47c59262867143a9ab103e6b
b56661 <chaos2805@gmail.com>
2019-06-26 17:36:13 +0800

    Chat conversation bubble

--- app/src/main/java/com/mazetireal/chat/MessageAdapter.java
+++ app/src/main/java/com/mazetireal/chat/MessageAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,15 +22,17 @@ import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-/**
- * Created by AkshayeJH on 24/07/17.
- */
 
-public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>{
+public class MessageAdapter extends RecyclerView.Adapter {
 
 
     private List<Messages> mMessageList;
     private DatabaseReference mUserDatabase;
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private String mCurrentUserId = mAuth.getCurrentUser().getUid();
 
     public MessageAdapter(List<Messages> mMessageList) {
 
@@ -38,24 +41,122 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     }
 
     @Override
-    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public int getItemCount() {
+        return mMessageList.size();
+    }
 
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.message_single_layout ,parent, false);
+    @Override
+    public int getItemViewType(int position) {
+        Messages c = mMessageList.get(position);
 
-        return new MessageViewHolder(v);
+        if (mCurrentUserId.equals(c.getFrom())) {
+            // If the current user is the sender of the message
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            // If some other user sent the message
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_single_incoming, parent, false);
+            return new SentMessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_single_layout, parent, false);
+            return new ReceivedMessageHolder(view);
+        }
+
+        return null;
 
     }
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
+    // Passes the message object to a ViewHolder so that the contents can be bound to UI.
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Messages c = mMessageList.get(position);
 
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((SentMessageHolder) holder).bind(c);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(c);
+        }
+    }
+
+    public class SentMessageHolder extends RecyclerView.ViewHolder {
+        public TextView messageText;
+        public TextView displayName;
+        public ImageView messageImage;
+        public TextView timeText;
+
+        SentMessageHolder(View view) {
+            super(view);
+
+            messageText = view.findViewById(R.id.message_text_layout);
+            displayName = view.findViewById(R.id.name_text_layout);
+            messageImage = view.findViewById(R.id.message_image_layout);
+            timeText = view.findViewById(R.id.time_text_layout);
+        }
+
+        void bind(Messages message) {
+            String from_user = message.getFrom();
+            String message_type = message.getType();
+            long message_time = message.getTime();
+            Log.d("APPTAG", "Text: " + message_time);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String finalDate = outputFormat.format(message_time);
+
+            mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
+
+            mUserDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    String name = dataSnapshot.child("name").getValue().toString();
+
+                    displayName.setText(name);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            timeText.setText(finalDate);
+
+            if (message_type.equals("text")) {
+
+                messageText.setText(message.getMessage());
+                messageImage.setVisibility(View.INVISIBLE);
+
+            } else {
+
+                messageText.setVisibility(View.INVISIBLE);
+                Picasso.get().load(message.getMessage())
+                        .placeholder(R.drawable.ic_avatar).into(messageImage);
+            }
+
+            messageText.setText(message.getMessage());
+        }
+    }
+
+    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
         public TextView messageText;
         public CircleImageView profileImage;
         public TextView displayName;
         public ImageView messageImage;
         public TextView timeText;
 
-        public MessageViewHolder(View view) {
+        ReceivedMessageHolder(View view) {
             super(view);
 
             messageText = view.findViewById(R.id.message_text_layout);
@@ -63,105 +164,53 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
             displayName = view.findViewById(R.id.name_text_layout);
             messageImage = view.findViewById(R.id.message_image_layout);
             timeText = view.findViewById(R.id.time_text_layout);
-
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(final MessageViewHolder viewHolder, int i) {
-
-        Messages c = mMessageList.get(i);
-
-        String from_user = c.getFrom();
-        String message_type = c.getType();
-        long message_time = c.getTime();
-        Log.d("APPTAG", "Text: " + message_time);
-//        Date myDate = new Date(message_time*1000);
-        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String finalDate = outputFormat.format(message_time);
-
-
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
-
-        mUserDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                String name = dataSnapshot.child("name").getValue().toString();
-                String image = dataSnapshot.child("thumb_image").getValue().toString();
-
-                viewHolder.displayName.setText(name);
-
-                Picasso.get().load(image)
-                        .placeholder(R.drawable.ic_avatar).into(viewHolder.profileImage);
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        viewHolder.timeText.setText(finalDate);
-
-        if(message_type.equals("text")) {
-
-            viewHolder.messageText.setText(c.getMessage());
-            viewHolder.messageImage.setVisibility(View.INVISIBLE);
-
-
-        } else {
-
-            viewHolder.messageText.setVisibility(View.INVISIBLE);
-            Picasso.get().load(c.getMessage())
-                    .placeholder(R.drawable.ic_avatar).into(viewHolder.messageImage);
-
         }
 
+        void bind(Messages message) {
+            String from_user = message.getFrom();
+            String message_type = message.getType();
+            long message_time = message.getTime();
+            Log.d("APPTAG", "Text: " + message_time);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String finalDate = outputFormat.format(message_time);
+
+
+            mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
+
+            mUserDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    String image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                    displayName.setText(name);
+
+                    Picasso.get().load(image)
+                            .placeholder(R.drawable.ic_avatar).into(profileImage);
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            timeText.setText(finalDate);
+
+            if (message_type.equals("text")) {
+
+                messageText.setText(message.getMessage());
+                messageImage.setVisibility(View.INVISIBLE);
+
+            } else {
+
+                messageText.setVisibility(View.INVISIBLE);
+                Picasso.get().load(message.getMessage())
+                        .placeholder(R.drawable.ic_avatar).into(messageImage);
+            }
+        }
     }
-
-    @Override
-    public int getItemCount() {
-        return mMessageList.size();
-    }
-
-
-
-
-
 
 }
-
-
-//    // Different types of rows
-//    private static final int TYPE_ITEM_LEFT = 0;
-//    private static final int TYPE_ITEM_RIGHT = 1;
-//    private static final int TYPE_ITEM_DATE_CONTAINER = 2;
-//
-//public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-//    class ViewHolder0 extends RecyclerView.ViewHolder {
-//        // Viewholder for row type 0
-//    }
-//
-//    class ViewHolder1 extends RecyclerView.ViewHolder {
-//        // Viewholder for row type 1
-//    }
-//
-//    class ViewHolder2 extends RecyclerView.ViewHolder {
-//        // Viewholder for row type 2
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, int position) {
-//        if (viewHolder.getItemViewType() == TYPE_ITEM_LEFT) {
-//            // Code to populate type 0 view here
-//
-//        } else if (viewHolder.getItemViewType() == TYPE_ITEM_RIGHT) {
-//            // Code to populate type 1 view here
-//
-//        } else if (viewHolder.getItemViewType() == TYPE_ITEM_DATE_CONTAINER) {
-//            // Code to populate type 2 view here
-//
-//        }
-//    }
